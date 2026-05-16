58d8e76a93b2c5a0274cd9c3171e50edb25134be
Umair khan <ukkhan5476@gmail.com>
2026-05-16 23:48:35 +0530

    fix(app): correct import order and syntax for post router

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
-import authRouter from "./routes/aurth.route.js";
-
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
+import authRouter from "./routes/aurth.route.js";
+import postRouter from "./routes/post.route.js";
+
 app.use("/api/auth", authRouter);
+app.use(("/api/post", postRouter));
 export default app;
