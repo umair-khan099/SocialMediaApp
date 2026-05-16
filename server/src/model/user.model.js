a807421378092fba68ddb0c0818481f7da7e309f
Umair khan <ukkhan5476@gmail.com>
2026-05-16 05:58:08 +0530

    feat(model): create user model with schema for user data

---
+++ server/src/model/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    unique: [true, "User Name Already Exist"],
+    required: [true, "User Name is Required"],
+  },
+
+  email: {
+    type: String,
+    unique: [true, "Email Already Exist"],
+    required: [true, "Email is Required"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Password is Required  "],
+  },
+
+  bio: {
+    type: String,
+  },
+
+  profileImage: {
+    type: String,
+  },
+});
+
+export const User = mongoose.model("user", userSchema);
