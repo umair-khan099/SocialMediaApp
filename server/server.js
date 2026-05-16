ca8b5800546384d2de9b44e7ab1afee688f792a9
Umair khan <ukkhan5476@gmail.com>
2026-05-16 05:58:15 +0530

    feat(server): establish database connection and enhance app middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import app from "./src/app.js";
 import CONFIG from "./src/config/dotenv.config.js";
+import { dnConnect } from "./src/config/db.config.js";
 
+dnConnect();
 app.listen(CONFIG.PORT, () => {
   console.log(`Server is running on PORT ${CONFIG.PORT}`);
 });
--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+import authRouter from "./routes/aurth.route.js";
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
 
-
+app.use("/api/auth", authRouter);
 export default app;
-
