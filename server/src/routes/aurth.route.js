9d99b5e9236fbf9a39e3f3e820759acc48b96020
Umair khan <ukkhan5476@gmail.com>
2026-05-17 06:47:29 +0530

    refactor(routes): remove unused auth route file

--- server/src/routes/aurth.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { login, register } from "../controllers/auth.controller.js";
-
-const authRouter = Router();
-
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-
-export default authRouter;
